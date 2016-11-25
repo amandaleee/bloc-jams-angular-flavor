fef6808ead3ec3189a6c925091ad80de918d9017
amandalee anderson <amandalee.anderson@gmail.com>
2016-11-24 21:27:29 -0600

    updating some comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,11 @@
 (function() {
-  //stateprovider  = state behavior
-  //locationprovider = how application handles urls - part of ng core 
 
   function config($stateProvider, $locationProvider) {
     $locationProvider
-    //this disables hashbang mode
       .html5Mode({
         enabled: true,
         requireBase: false
       });
-    // $urlRouterProvider.otherwise('/'); - was jus trying a thing there
-    //stateprovider calls the state method w two arguments - stateName and stateConfig 
-    //like this - $stateProvider.state(stateName, stateConfig)
     $stateProvider
       .state('landing', {
         url: '/',
@@ -21,12 +15,8 @@
         url: '/album',
         templateUrl: 'templates/album.html'
       });
-
   }
 
-
-// the [] below contain dependency injections - external modules/utilities that this function requires to run
-//add the script to index.html 
   angular
     .module('blocJams', ['ui.router'])
     .config(config);
