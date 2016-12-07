e83e1c7ff0f087f8bdcb65cace53fdca773a84be
amandaleee <amandalee@amandalee.org>
2016-12-06 21:11:44 -0600

    some spacing stuff because 4-space tabs are driving me up a wall

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,29 +1,11 @@
 (function() {
+
   function AlbumCtrl() {
     this.albumData = albumPicasso;
-   debugger;
-    // this.songs = [];
-    // this.title = "YUP";
-    // //song in album.songs
-    // for (var i=0; i < 12; i++) {
-    //   this.songs.push(angular.copy(this.title));
-    // }
-    // this.albumData =  {
-    //   title: 'The Colors',
-    //   artist: 'Pablo Picasso',
-    //   label: 'Cubism',
-    //   year: '1881',
-    //   albumArtUrl: 'assets/images/album_covers/01.png',
-    //   songs: [
-    //     { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
-    //     { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
-    //     { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-    //     { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink'},
-    //     { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta'}
-    //   ]
-    // };
   }
+
   angular
     .module('blocJams')
     .controller('AlbumCtrl', AlbumCtrl);
+    
 })();
